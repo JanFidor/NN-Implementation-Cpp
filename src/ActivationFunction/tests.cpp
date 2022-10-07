f25bb846c458c1f3c8865def6a5d34fa34fa8e82
JanFidor <jan.p.fidor@gmail.com>
2022-10-07 18:37:10 +0200

    add tests for calculating a derivative

--- src/ActivationFunction/tests.cpp
+++ src/ActivationFunction/tests.cpp
@@ -23,3 +23,23 @@ TEST_CASE("Sigmoid function correctly calculates output for a big negative numbe
     bool condition = aproximatelyEqual(f.calculateOutput(-99999), 0.0, 0.0001);
     REQUIRE(condition);
 }
+
+
+TEST_CASE("Sigmoid function correctly calculates derivative for 0"){
+    SigmoidFunction f;
+    bool condition = aproximatelyEqual(f.calculateDerivative(0), 0, 0.0001);
+    REQUIRE(condition);
+}
+
+
+TEST_CASE("Sigmoid function correctly calculates derivative for 1"){
+    SigmoidFunction f;
+    bool condition = aproximatelyEqual(f.calculateDerivative(1), 0.0, 0.0001);
+    REQUIRE(condition);
+}
+
+TEST_CASE("Sigmoid function correctly calculates derivative for a negative number"){
+    SigmoidFunction f;
+    bool condition = aproximatelyEqual(f.calculateDerivative(-3), -12.0, 0.0001);
+    REQUIRE(condition);
+}
